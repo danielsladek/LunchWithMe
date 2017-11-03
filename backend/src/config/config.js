0a3df77c3f293de2fd5aafe33e6b92fc9008f546
Marek Novy <mnovy@profesionalita.cz>
2017-11-03 15:04:21 +0100

    backend pridani gitignore

---
+++ backend/src/config/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "development": {
+    "username": "xnovm184",
+    "password": "xc4c94x6aebk65s9",
+    "database": "user_xnovm184",
+    "host": "vse.handson.pro",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "xnovm184",
+    "password": "xc4c94x6aebk65s9",
+    "database": "user_xnovm184",
+    "host": "vse.handson.pro",
+    "dialect": "mysql"
+  }
+};
