324b4157a791c9761b9725acad644facd891873f
Jakub Kaspar <mr.kaspi@gmail.com>
2017-11-03 15:53:51 +0100

    Added router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import { RegisterForm } from './components/registerForm.js';
+import Landing from './pages/Landing.js';
+import Registration from './pages/registration.js';
+import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router'
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router history={browserHistory}>
+        <Route path="/" component={Landing}>
+          <Route path="reg" component={Registration} />
+        </Route>
+      </Router>
     );
   }
 }
