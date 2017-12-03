bd0250a46cc839ed5ddcf8ca8f849aad146056e0
Daniel Sládek <me@danielsladek.cz>
2017-12-03 23:50:39 +0100

    fake profile image

--- frontend/src/components/LunchBuddyIcon.js
+++ frontend/src/components/LunchBuddyIcon.js
@@ -3,29 +3,38 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 import { Link } from 'react-router';
+import graph from 'fb-react-sdk';
 
 export class LunchBuddyIcon extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      url: null
+    }
+  }
+
+  componentDidMount() {
+  
+    let url;
+    const { facebookId } = this.props.lunchBuddy;
+    fetch('https://graph.facebook.com/v2.11/' + facebookId + '/picture')
+    .then(response => {
+        this.state.url = response.url
+    });
+  }
 
   render() {
 
     const { name, surname, icon, id, facebookId } = this.props.lunchBuddy;
 
-    FB.api(
-      '/'+ facebookId +'/picture',
-      'GET',
-      {
-        'height': 100,
-        'width': 100
-      },
-      function(response) {
-        console.log( 'response' );
-      }
-    );
-
+    console.log('>>> url state', this.state.url)
 
     return (
+
       <div className="lunchBuddyIcon">
-        
+        <img src="https://static.xx.fbcdn.net/rsrc.php/v3/yo/r/UlIqmHJn-SK.gif" />
       </div>
     );
   }
