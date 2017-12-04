75745365f48bdc57abe69ff79c64f441e5b98a5e
Marek Novy <mnovy@profesionalita.cz>
2017-12-04 16:54:06 +0100

    mizeni with u attendees

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,14 +1,14 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "adminadmin",
+    "password": "",
     "database": "lunchwithme",
     "host": "localhost",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": "adminadmin",
+    "password": "",
     "database": "lunchwithme",
     "host": "localhost",
     "dialect": "mysql"
