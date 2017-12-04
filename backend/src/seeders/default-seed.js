4034bc221f63ca92c3cc961fa9ac19d4ad186b30
mrkaspi <jakub.jk.kaspar@gmail.com>
2017-12-04 17:34:36 +0100

    Updated seeds

--- backend/src/seeders/default-seed.js
+++ backend/src/seeders/default-seed.js
@@ -11,8 +11,8 @@ module.exports = {
       [
         { createdAt: new Date(), updatedAt: new Date(), id: 1, name: 'John', icon: 'https://static.xx.fbcdn.net/rsrc.php/v3/yo/r/UlIqmHJn-SK.gif', surname: 'Doe', email: 'john.doe@lunchwithme.cz', facebookId: '555555555' },
         { createdAt: new Date(), updatedAt: new Date(), id: 2, name: 'Tomas', icon: 'https://static.xx.fbcdn.net/rsrc.php/v3/yo/r/UlIqmHJn-SK.gif', surname: 'Fenyk', email: 'tomas@lunchwithme.cz', facebookId: '666666666' },
-        { createdAt: new Date(), updatedAt: new Date(), id: 3, name: 'Marek', icon: 'https://static.xx.fbcdn.net/rsrc.php/v3/yo/r/UlIqmHJn-SK.gif', surname: 'Novy', email: 'marek@lunchwithme.cz', facebookId: '777777777' },
-        { createdAt: new Date(), updatedAt: new Date(), id: 4, name: 'Daniel', icon: 'https://static.xx.fbcdn.net/rsrc.php/v3/yo/r/UlIqmHJn-SK.gif', surname: 'Sladek', email: 'daniel@lunchwithme.cz', facebookId: '88888888' },
+        { createdAt: new Date(), updatedAt: new Date(), id: 3, name: 'Marek', icon: 'https://scontent.xx.fbcdn.net/v/t1.0-1/p80x80/14222227_10209192314719103_8607768522029368925_n.jpg?oh=d3b76abc34211ae1a49abfb4c4b8de6d&oe=5A9BDFD2', surname: 'Novy', email: 'marek@lunchwithme.cz', facebookId: '10213238493511044' },
+        { createdAt: new Date(), updatedAt: new Date(), id: 4, name: 'Daniel', icon: 'https://scontent.xx.fbcdn.net/v/t1.0-1/p50x50/12744762_10205555401075372_3323371200062319048_n.jpg?oh=1f605c6cf68c7622cff3de34277cd31c&oe=5A99C640', surname: 'Sladek', email: 'daniel@lunchwithme.cz', facebookId: '10210121902915064' },
       ],
       {}
     );
@@ -30,7 +30,7 @@ module.exports = {
     const eventPromise = queryInterface.bulkInsert(
       'Events',
       [
-        { createdAt: new Date(), updatedAt: new Date(), placeId: 1, maximumLunchBuddies: 5, organizatorId: 1, description: 'Default event description.', timeStart: new Date(), timeEnd: addSeconds(new Date(), 60*Math.random() * (max - min) + min) },
+        { createdAt: new Date(), updatedAt: new Date(), placeId: 1, maximumLunchBuddies: 5, organizatorId: 4, description: 'Default event description.', timeStart: new Date(), timeEnd: addSeconds(new Date(), 60*Math.random() * (max - min) + min) },
         { createdAt: new Date(), updatedAt: new Date(), placeId: 2, maximumLunchBuddies: 5, organizatorId: 2, description: 'Default event description.', timeStart: new Date(), timeEnd: addSeconds(new Date(), 60*Math.random() * (max - min) + min) },
         { createdAt: new Date(), updatedAt: new Date(), placeId: 3, maximumLunchBuddies: 5, organizatorId: 3, description: 'Default event description.', timeStart: new Date(), timeEnd: addSeconds(new Date(), 60*Math.random() * (max - min) + min) },
       ],
@@ -44,6 +44,7 @@ module.exports = {
         { createdAt: new Date(), updatedAt: new Date(), userId: 2, eventId: 1, invited: 0, willAttend: 1 },
         { createdAt: new Date(), updatedAt: new Date(), userId: 2, eventId: 2, invited: 0, willAttend: 1 },
         { createdAt: new Date(), updatedAt: new Date(), userId: 3, eventId: 2, invited: 0, willAttend: 1 },
+        { createdAt: new Date(), updatedAt: new Date(), userId: 4, eventId: 2, invited: 0, willAttend: 1 },
       ],
       {}
     );
